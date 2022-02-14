64a06902376792da1b33277e161526e84e32653f
Zoran Đukić <zorandjukic.os@gmail.com>
2022-02-14 15:57:13 +0100

    Hardcoded information. Next is adding using YELP api

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,20 +5,17 @@ import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
     render() {
-        return () => {
-            <div class="BusinessList">
-                <Business  />
-                <Business  />
-                <Business  />
-                <Business  />
-                <Business  />
-                <Business  />
-            </div>
-
-        }
-
-
+      return (
+        <div className="BusinessList">
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+          <Business />
+        </div>
+      );
     }
-}
-
-export default BusinessList;
+  }
+  
+  export default BusinessList;
